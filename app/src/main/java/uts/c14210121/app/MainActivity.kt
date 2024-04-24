e692fbfb25ee3f4830e16bad592e6d5fb2bcbbb9
NicholasL5 <nicholas1s.phang@gmail.com>
2024-04-24 14:18:14 +0700

    halaman beranda

--- app/src/main/java/uts/c14210121/app/MainActivity.kt
+++ app/src/main/java/uts/c14210121/app/MainActivity.kt
@@ -1,12 +1,15 @@
 package uts.c14210121.app
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.*
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    lateinit var _btnhalpemain : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +19,13 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        _btnhalpemain = findViewById<Button>(R.id.btnhalpemain)
+
+        _btnhalpemain.setOnClickListener{
+            var i = Intent(this@MainActivity, DataPemain::class.java)
+            startActivity(i)
+        }
+
     }
 }
